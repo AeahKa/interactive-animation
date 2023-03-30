d4f6464238e65464878fdd2e3d11f977ddf1bed3
AeahKa <aeah@qq.com>
2023-03-30 15:36:37 +0800

    确定Scenes组件的结构

--- src/components/Scenes.vue
+++ src/components/Scenes.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    轮换动画
+  <div class="scenes-wrapper">
+    <div class="scenes-item">
+      111111
+    </div>
+    <div class="scenes-item">
+      222
+    </div>
+    <div class="scenes-item">
+      333
+    </div>
   </div>
 </template>
 
