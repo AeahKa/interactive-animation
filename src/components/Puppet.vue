62581b8286a46d086412c890de88a4b2f2ebd7ee
AeahKa <aeah@qq.com>
2023-04-01 10:32:35 +0800

    创建Puppet.vue

---
+++ src/components/Puppet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="puppet">
+    <div class="puppet-head"></div>
+    <div class="puppet-body"></div>
+    <div class="puppet-arm">
+      <div class="puppet-arm-left"></div>
+      <div class="puppet-arm-right"></div>
+    </div>
+    <div class="puppet-leg">
+      <div class="puppet-leg-left"></div>
+      <div class="puppet-leg-right"></div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="js">
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.puppet{
+  .puppet-head{}
+  .puppet-body{}
+  .puppet-arm{
+    .puppet-arm-left{}
+    .puppet-arm-right{}
+  }
+  .puppet-leg{
+    .puppet-leg-left{}
+    .puppet-leg-right{}
+  }
+}
+</style>
